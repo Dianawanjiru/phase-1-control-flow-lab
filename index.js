8781b187fe9b8235a39ab9370b4122dbed6f1e13
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-17 14:47:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+
+  if(someValue <=400){
+    return `This one is on me!`;
+    
+  }
+  else if (someValue > 2000){
+    if (someValue >= 2500){
+      return "No can do."};
+
+    return "I will gladly take your thirty bucks.";
+  
+  
+  };
+};
+  
+  // Write your code here!
+
+
+
+function ternaryCheckCity(checkCity){
+  
+  if (checkCity === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else if (checkCity !== 'NYC'){
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip){ case "generous":
+    return "Thank you so much."
 
-function switchOnCharmFromTip(){
+    break;
+    case "not as generous":
+      return "Thank you."
+      break;
+      default:
+        return "Bye."
+  }
+  
+  }
   // Write your code here!
-}
